4a66d2f741895a43a97da10d69dfc0c47d9338c4
NickRoccodev11 <nchls.rcc@gmail.com>
2023-02-28 17:15:43 -0500

    server file, package.json files, etc

--- frontend/src/components/PostList.jsx
+++
@@ -1,7 +0,0 @@
-import PostPreview from './PostPreview';
-
-const PostList = ({ posts }) => <ul className="row list-unstyled">
-	{posts.map(post => <PostPreview key={post._id} {...post} />)}
-</ul>
-
-export default PostList;
