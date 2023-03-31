10b6a2e4d24b5c006d2ffb50d7ebb95afaaf3242
NickRoccodev11 <nchls.rcc@gmail.com>
2023-03-31 13:34:43 -0400

    updated ejs to JSX

---
+++ views/Feed.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Main from './components/Main'
+
+function Feed({posts}) {
+    return (
+        <Main>
+            <div class="container">
+                <div class="row justify-content-center mt-5">
+                    <ul class="row list-unstyled">
+                        {posts.map(post =>
+                            <li className="col-6 justify-content-between mt-5">
+                                <a href={`/post/${post._id}`}>
+                                    <img className="img-fluid" src={post.image} />
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+
+        </Main>
+    )
+}
+
+export default Feed
---
+++ views/Login.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Main from './components/Main'
+
+function Login({messages}) {
+    return (
+        <Main>
+            <main className="container">
+                <div className="row justify-content-center">
+                    <section className="col-6 mt-5">
+
+                        {messages.errors
+                            ? messages.errors.map((el, i) =>
+                                <div key={i} className="alert alert-danger">{el.msg} </div>
+                            )
+                            : null
+                        }
+                        {messages.info
+                            ? messages.info.map((el, i) =>
+                                <div key={i} className="alert alert-danger">{el.msg} </div>
+                            )
+                            : null
+                        }
+                        <form action="/login" method="POST">
+                            <div className="mb-3">
+                                <label for="exampleInputEmail1" className="form-label"
+                                >Email address</label
+                                >
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleInputEmail1"
+                                    aria-describedby="emailHelp"
+                                    name="email"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label for="exampleInputPassword1" className="form-label">Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id="exampleInputPassword1"
+                                    name="password"
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </section>
+                </div>
+            </main>
+
+        </Main>
+    )
+}
+
+export default Login
